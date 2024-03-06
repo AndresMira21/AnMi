7775c5dfe3125ab1151e9f5d0eff53e31d0d66fb
AndresMira21 <160984856+AndresMira21@users.noreply.github.com>
2024-03-06 12:53:53 -0500

    Add files via upload

---
+++ SIMULADOR_BANCARIO/SimuladorBancario/CuentaCorriente.py
@@ -0,0 +1,23 @@
+class CuentaCorriente:
+    # Aqui va el codigo del empleado
+    '''--------------------------------
+    # Atributos
+    --------------------------------'''
+    saldo = ""
+
+    '''--------------------------------------
+    # Metodos
+    --------------------------------------'''
+
+    def ConsultarSaldo(self):
+        return self.saldo
+
+    def ConsigarValor(self, saldoCorriente):
+        nsaldoCorriente = self.saldoCorriente + ''
+        self.saldoCorriente = nsaldoCorriente
+        return ''
+    
+    def RetirarValor(self, saldoCorriente):
+        nsaldoCorriente = self.saldoCorriente * ''
+        self.saldoCorriente = nsaldoCorriente
+        return ''
