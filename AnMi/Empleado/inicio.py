5e8dfb1c1dd1c032e72f7c5233721c0796d62a53
AndresMira21 <160984856+AndresMira21@users.noreply.github.com>
2024-05-19 19:12:29 -0500

    Delete AnMi directory

--- AnMi/Empleado/inicio.py
+++
@@ -1,7 +0,0 @@
-from Empleado import Empleado
-
-empleado1 = Empleado('Juan', 'Perez', 1, 1000000)
-empleado2 = Empleado('Pedro', 'Caicedo', 1, 2000000)
-
-print(empleado1.ConsultarSalario())
-print(empleado2.ConsultarNombre())
